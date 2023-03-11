ff824877f4261cf95327d23a765c811e2d76876b
Balajiguvi <balaviji016@gmail.com>
2023-03-11 20:22:07 +0530

    First commit

---
+++ src/Play.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Play() {
+    return (
+        <div>
+            <h1>Welcome to the Gaming World...🤩😎</h1>
+            <button className='btn btn-primary'>Play</button>
+        </div>
+    )
+}
+
+export default Play
